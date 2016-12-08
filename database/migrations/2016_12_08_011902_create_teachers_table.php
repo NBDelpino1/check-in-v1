1a2f29d03020ef0ad6825036dd0a21e08bf373ac
Nicholas Delpino <Nicky@Nicholas-MacBook-Pro.local>
2016-12-07 20:20:10 -0500

    created teachers model controller resource

--- database/migrations/2016_12_08_011902_create_teachers_table.php
+++ database/migrations/2016_12_08_011902_create_teachers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStudentsTable extends Migration
+class CreateTeachersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,8 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('teachers', function (Blueprint $table) {
             $table->increments('id');
-            $table->text('assignment');
-            $table->dateTime('due');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('teachers');
     }
 }
